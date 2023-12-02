5689393dda897f5b244e9bfad87e051e73fdbc44
geekendev <justin@watsonnetworks.com>
2023-12-02 05:44:18 -0600

    testing out RustRover.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ * Copyright (c) 2023 - Chatter Social, Inc.
+ * Created by: Justin B. Watson (Geeken)
+ */
+
+use serde::Deserialize;
+use std::fmt::Debug;
+use surrealdb::engine::remote::ws::Wss;
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
+
+#[derive(Debug, Deserialize)]
+struct User {
+    #[allow(dead_code)]
+    id: Thing,
+    username: String,
+    first_name: Option<String>,
+    last_name: Option<String>,
+    email: String,
+    email_verified: Option<bool>,
+}
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    // Connect to the server
+    let db = Surreal::new::<Wss>("dev-db.chattersocial.io").await?;
+
+    // Sign in as a namespace, database, or root user
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
+
+    // Select a specific namespace / database
+    db.use_ns("dev").use_db("dev").await?;
+
+    // Select all people records
+    let people: Vec<User> = db.select("user").await?;
+
+    let filtered_users: Vec<_> = people
+        .iter()
+        .filter(|user| user.email_verified == Some(true))
+        .collect();
+    let mut user = filtered_users[0];
+    println!("{:?}", user.id.to_raw());
+
+    Ok(())
 }
